24e197d313887b7a4d569f7b5d87bc0261f29444
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-11 16:21:55 -0600

    basic statements exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,10 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-    artist VARCHAR(50),
-    name VARCHAR (50),
-    release_date INT,
-    sales FLOAT,
+    artist VARCHAR(50) NOT NULL DEFAULT 'unkown',
+    name VARCHAR (100) NOT NULL DEFAULT 'untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
     genre VARCHAR(255)
 );
+

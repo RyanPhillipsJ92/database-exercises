ac5c2548b324a04f82e54e8386583a858d9257f9
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-14 09:03:10 -0600

    working on group by lecture

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -9,5 +9,5 @@ select concat(first_name, ' ', last_name) as 'Employee',
        datediff(current_date(), hire_date) as 'Days worked at Company'
 from employees
 where hire_date like '199%'
-  and (birth_date like '%-12-25')
+and (birth_date like '%-12-25')
 order by birth_date asc, hire_date desc;
